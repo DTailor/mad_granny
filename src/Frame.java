414a44b83ecb971ef1c377ecf796fdc7dfae7364
Dan Tailor <dann.croitoru@gmail.com>
2013-06-15 13:23:28 +0300

    sample frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,16 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame{
-	public static void main(String args[]){
-		Frame frame = new Frame();
+	public static String title = "Mad Grannies";
+	public static Dimension size = new Dimension(600,500);
+	
+	
+	public Frame(){
+		setTitle(title);
+		setSize(size);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		init();
 	}
 	
 	
 	public void init(){
-		
+		setVisible(true);
 	}
 	
-	public Frame(){
-		
+	public static void main(String args[]){
+		Frame frame = new Frame();
 	}
+	
 }
